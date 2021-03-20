93240b3e3595f3d64d182f984790f99920517fa6
RamzSt1 <Ira455436@gmail.com>
2021-03-20 11:47:38 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Components/Kit/Header/"
+
+function App() {
+  return (
+    <div className="App">
+      <Header>
+      
+      </Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Kit/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Logotip from './standart.svg'
+import Mess from './Group31.svg'
+import Call from './Group32.svg'
+import Logotip1 from './Bubble.svg'
+import Logotip2 from './Emily.svg'
+import Logotip3 from './Ellipse 1.svg'
+import Logotip4 from './Rectangle52.svg'
+
+
+export default function Header() {
+    return (
+        <div>
+            <img src={Logotip}/>
+            <img src={Mess}/>
+            <img src={Call}/>
+            <img src={Logotip1}/>
+            <img src={Logotip2}/>
+            <img src={Logotip3}/>
+            <img src={Logotip4}/>
+        </div>
+    )
+}
